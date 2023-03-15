be3b8edfc5b698f1efc50c4c10077340bb57f4a4
MangataTS <1196991321@qq.com>
2023-03-16 01:37:32 +0800

    更新 ProxyHttp

--- Core/ProxyHttp.go
+++ Core/ProxyHttp.go
@@ -155,6 +155,7 @@ func (i *ProxyHttp) Transport(request *http.Request) (*http.Response, error) {
 	i.RemoveHeader(request.Header)
 	response, err := (&http.Transport{
 		DisableKeepAlives:     true,
+		TLSHandshakeTimeout:   5 * time.Second,
 		ResponseHeaderTimeout: 60 * time.Second,
 		DialContext:           i.DialContext(),
 		TLSClientConfig:       &tls.Config{InsecureSkipVerify: true},
@@ -453,7 +454,7 @@ func (i *ProxyHttp) DialContext() func(ctx context.Context, network, addr string
 			}
 		}
 		tcpAddr, _ := net.ResolveTCPAddr("tcp", ip+addr[separator:])
-		conn, err = net.DialTCP("tcp", nil, tcpAddr)
+		conn, err = net.DialTimeout("tcp", tcpAddr.String(), time.Duration(5)*time.Second)
 		if err != nil {
 			return conn, err
 		}
